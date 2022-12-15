f60daa0334f40b12a6dc7d009217310ed8536448
Dylan-Stroud <dylan@strouds.net>
2022-12-15 21:43:13 +1000

    added full planner times from 9-5, added logic for saving through reloads, added logic for date time, allowing for time to be properly recordedand saved

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@
 // in the html.
 $(function () {
   
-
+  var timeSlots = $(".time-block");
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,14 +21,19 @@ $(function () {
     //TODO: save to textContent, this.parent perhaps, 
     var timeDiv = $(this).parent();
     
+    
 
     var noteCont = $(timeDiv).children(".description");
     
 
 
     var note = $(noteCont).val();
-    localStorage.setItem(timeDiv, note);
-    console.log(note);
+    console.log("note " + note);
+    localStorage.setItem(timeDiv.attr("id"), note);
+    console.log("Saved!!!")
+    console.log(timeDiv.attr("id"));
+    console.log(localStorage.getItem(timeDiv.attr("id")));
+    
     
   }
 
@@ -38,15 +43,65 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+
+  currenthour = (dayjs().format("HH") - 1);
+  console.log(currenthour);
+ 
+
+  for(var i =0; i<timeSlots.length; i++){
+    currentSlot = $(timeSlots[i]);
+    timeSlotHour = currentSlot.attr("id");
+    timeSlotHour = timeSlotHour.split("-")[1];
+    if(currenthour == timeSlotHour){
+      console.log("lessthan " + currenthour + ", " + timeSlotHour);
+      currentSlot.addClass("present");
+      currentSlot.removeClass("past");
+      currentSlot.removeClass("future");
+    }
+    if(currenthour < timeSlotHour){
+      console.log("lessthan " + currenthour + ", " + timeSlotHour);
+      currentSlot.addClass("future");
+      currentSlot.removeClass("present");
+      currentSlot.removeClass("past");
+    }
+    if(currenthour > timeSlotHour){
+      console.log("lessthan " + currenthour + ", " + timeSlotHour);
+      currentSlot.addClass("past");
+      currentSlot.removeClass("present");
+      currentSlot.removeClass("future");
+    }
+  }
+
+
+
+
   
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  timeSlots = $(".description");
-  alert(timeSlots.length);
   
 
+  for(var i = 0; i< timeSlots.length; i++){
+    currentSlot= $(timeSlots[i]);
+    timeTxtBox = currentSlot.children(".description");
+    timeId = timeSlots[i].id;
+    console.log("requestID " + timeId);
+    console.log(getSavedValue(timeId));
+
+    $(timeTxtBox).val(getSavedValue(timeId));
+
+  }
+
+   //get the saved value function - return the value of "val" from the localStorage. 
+   function getSavedValue  (val){
+    if (!localStorage.getItem(val)) {
+        return "";//default value
+        
+    }
+    return localStorage.getItem(val);
+    }
   // TODO: Add code to display the current date in the header of the page.
 
   function dateTime(){
